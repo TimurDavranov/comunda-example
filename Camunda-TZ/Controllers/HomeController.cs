7dbfe32eefed41e78d34868aced74e77a0ec78ac
TimurDavranov <timur.davranov8090@gmail.com>
2025-03-05 21:01:52 +0500

    fix

--- Camunda-TZ/Controllers/HomeController.cs
+++ Camunda-TZ/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Camunda_TZ.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Camunda_TZ.Controllers;
 
 //[Authorize]
-public class HomeController : Controller
+public class HomeController(IConfiguration configuration) : Controller
 {
     public IActionResult Index()
     {
@@ -19,8 +22,19 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
-    public IActionResult Logout()
+    public async Task<IActionResult> Logout()
     {
-        return SignOut("Cookies", "OpenIdConnect");
+        var idToken = await HttpContext.GetTokenAsync(OpenIdConnectDefaults.AuthenticationScheme, "id_token");
+        
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        var authUrl = configuration["Keycloak:Authority"]?.TrimEnd('/').TrimEnd();
+
+        var logoutUrl = $"{authUrl}/protocol/openid-connect/logout";
+        var postLogoutRedirectUri = Url.Action("Index", "Home", null, "http");
+
+        var logoutRequest = $"{logoutUrl}?id_token_hint={idToken}&post_logout_redirect_uri={Uri.EscapeDataString(postLogoutRedirectUri)}";
+
+        return Redirect(logoutRequest);
     }
 }
